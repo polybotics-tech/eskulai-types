e2924578a49619e2c934cf287676be2673b39d9a
polybotics-tech <polyboticstech@gmail.com>
2026-08-02 19:05:21 +0100

    add: drug search/result types

---
+++ src/types/drug.ts
@@ -0,0 +1,107 @@
+import { DefaultContentListType } from "@/types/global.js";
+
+export enum DrugSuggestionTypeEnum {
+  Drug = "drug",
+  Brand = "brand",
+  Constituent = "constituent",
+}
+
+export type DrugSearchSuggestionType = {
+  type: DrugSuggestionTypeEnum;
+  name: string;
+
+  tags: string[]; //--(for type=drug -> will be list of drug classification/actions, for type=brand -> will be list of popular drug names from the brand, for type=constituent -> will be list of common drugs it can be found in)
+};
+
+export type DrugSearchResultIsDrug = {
+  type: "drug";
+
+  name: string;
+  genericName?: string;
+
+  pronunciation?: string;
+
+  aliases: string[];
+  classifications: string[];
+
+  prescriptionStatus: "OTC" | "Prescription" | "Controlled";
+
+  summary: DefaultContentListType;
+
+  indications: DefaultContentListType;
+  dosage: DefaultContentListType;
+  activeConstituents: string[];
+
+  administration: DefaultContentListType;
+
+  contraindications: DefaultContentListType;
+  warnings: DefaultContentListType;
+  sideEffects: DefaultContentListType;
+  overdose: DefaultContentListType;
+
+  pregnancy: DefaultContentListType;
+  breastfeeding: DefaultContentListType;
+
+  interactions: DefaultContentListType;
+  monitoring: DefaultContentListType;
+
+  storage: DefaultContentListType;
+
+  patientCounselling: DefaultContentListType;
+
+  relatedSuggestions: DrugSearchSuggestionType[];
+  references: DefaultContentListType;
+};
+
+type DrugSearchResultIsBrand = {
+  type: "brand";
+
+  name: string;
+
+  manufacturer: string;
+  country?: string;
+
+  availableStrengths: string[];
+  dosageForms: string[];
+  activeConstituents: string[];
+
+  summary: DefaultContentListType;
+
+  indications: DefaultContentListType;
+  administration: DefaultContentListType;
+  warnings: DefaultContentListType;
+
+  storage: DefaultContentListType;
+
+  relatedSuggestions: DrugSearchSuggestionType[];
+  references: DefaultContentListType;
+};
+
+type DrugSearchResultIsConstituent = {
+  type: "constituent";
+
+  name: string;
+
+  chemicalClass?: string;
+  molecularFormula?: string;
+  mechanismOfAction: DefaultContentListType;
+
+  pharmacokinetics: DefaultContentListType;
+  pharmacodynamics: DefaultContentListType;
+
+  indications: DefaultContentListType;
+  adverseEffects: DefaultContentListType;
+  contraindications: DefaultContentListType;
+  interactions: DefaultContentListType;
+  toxicity: DefaultContentListType;
+
+  commonBrands: string[];
+
+  relatedSuggestions: DrugSearchSuggestionType[];
+  references: DefaultContentListType;
+};
+
+export type DrugSearchResultType =
+  | DrugSearchResultIsDrug
+  | DrugSearchResultIsBrand
+  | DrugSearchResultIsConstituent;
