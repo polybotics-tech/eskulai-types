9d4984a5017ab676834d4bc63b931c373976564a
polybotics-tech <polyboticstech@gmail.com>
2026-09-12 09:24:20 +0100

    update: chat_sendMessage JOI schema

--- src/schemas/chat.ts
+++ src/schemas/chat.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const sendAIChatMessageSchema = Joi.object({
-  conversationId: Joi.string().uuid().required(),
+  conversationId: Joi.string().optional(),
   message: Joi.string().min(1).max(5000).required(),
 }).unknown(false);
