f2841bb2ad3e4de62c09d779d5a40f8637dc5854
polybotics-tech <polyboticstech@gmail.com>
2026-09-24 16:18:22 +0100

    fix: export drug response type

--- src/api/responses/index.ts
+++ src/api/responses/index.ts
@@ -1,5 +1,6 @@
 export * from "@/api/responses/auth.js";
 export * from "@/api/responses/diagnosis.js";
+export * from "@/api/responses/drug.js";
 export * from "@/api/responses/imaging.js";
 export * from "@/api/responses/user.js";
 export * from "@/api/responses/note.js";
